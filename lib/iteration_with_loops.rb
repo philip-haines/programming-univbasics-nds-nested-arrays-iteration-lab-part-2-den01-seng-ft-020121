86176f6dc5a078baa8a47a06c59ee6c09a818ba7
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-26 22:59:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def find_min_in_nested_arrays(src)
   
   while row_index < src.count do 
     element_index = 0
-    smallest_number = []
+    smallest_number = 100,000
     
     while element_index < src[row_index].count do
       if src[row_index][element_index] < smallest_number
